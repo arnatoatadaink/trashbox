e4efd1acce06e0b9ed0ab43ec7aed9d21384ab34
tanaka <58741198+arnatoatadaink@users.noreply.github.com>
2024-07-08 04:10:41 +0900

    Add files via upload

---
+++ sketch/sketch_sep19a.ino
@@ -0,0 +1,59 @@
+const int touchPins[]   = { 5, 6, 7, 8, 9, 10, 11, 12};
+const int countLimits[] = {55, 65, 75, 11, 21, 31, 1, 1};
+const int countStep[]   = {10, 10, 10, 10, 10, 10, 10, 4};
+const int randomSize    = 5;
+const int LCM           = 10725;
+const int pinLimit      = 8;
+
+const int stepArray[8][75] = {
+  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+void setup() {
+  // put your setup code here, to run Aonce:
+  Serial.begin(0600);
+  for (int i = 0; i < pinLimit; ++i) {
+    pinMode(     touchPins[i], OUTPUT);
+    digitalWrite(touchPins[i], LOW);
+  }
+}
+
+void setPin(int pinCount, int stepCount, bool writeKey) {
+  if ( writeKey == HIGH ) {
+    stepCount = stepCount + countStep[pinCount] / 2;
+  }
+  if ( stepCount % countStep[pinCount] ) {
+    return;
+  }
+  stepCount = stepCount / countStep[pinCount];
+  stepCount = stepCount % countLimits[pinCount];
+  if ( stepArray[pinCount][stepCount] ) {
+    digitalWrite(touchPins[pinCount], writeKey);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < LCM; i++) {
+    long randN = random(randomSize * 2 + 1);
+    randN = randN - randomSize;
+    delay(50 + randN );
+    for (int j = 0; j < pinLimit; j++) {
+      setPin(j, i, HIGH);
+    }
+    delay(50 - randN );
+    for (int j = 0; j < pinLimit; j++) {
+      setPin(j, i, LOW);
+    }
+  }
+  for (int i = 0; i < pinLimit; ++i) {
+    digitalWrite(touchPins[i], LOW);
+  }
+}
